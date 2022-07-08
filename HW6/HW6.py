d0891390cfbdbdd2a5b4f5deb8d49501bc489828
Regato <shingamidesu@gmail.com>
2022-07-08 04:37:26 -0400

    HW6 ReTake

--- HW6/HW6.py
+++ HW6/HW6.py
@@ -24,22 +24,24 @@ def cinema_age(value):
     str_value = str(value)
     str_reversed = ''.join(reversed(str_value))
     str_len = len(str_value)
+    str_list = [item for item in str_value if item == str_value[-1]]
+    list_len = len(str_list)
 
     # Check is value Integer or Float
     if int_bool and value >= 0:
-        if value < 7:
+        if value < 7 and not (str_value == str_reversed and str_len == list_len and str_len > 1):
             response_message = f'You are {value}! Where are your parents?'
 
             return response_message
-        elif value < 16 and not str_value == str_reversed:
+        elif value < 16 and not (str_value == str_reversed and str_len == list_len and str_len > 1):
             response_message = f'You are only {value}, but this film is adults only!'
 
             return response_message
-        elif value > 65 and not str_value == str_reversed:
+        elif value > 65 and not (str_value == str_reversed and str_len == list_len and str_len > 1):
             response_message = f'Are you {value}? Show your pension ID!'
 
             return response_message
-        elif str_value == str_reversed and str_len > 1:
+        elif str_value == str_reversed and str_len == list_len and str_len > 1:
             response_message = f'Oh you are {value}! What\'s the interesting age!'
 
             return response_message
