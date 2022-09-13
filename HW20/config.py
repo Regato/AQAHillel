79b404a591f58660ccc8628719de767eff940eb8
Regato <shingamidesu@gmail.com>
2022-09-13 20:06:25 +0300

    Pytest fix

--- HW20/config.py
+++ HW20/config.py
@@ -1,9 +1,3 @@
-import json
-
-import pytest
-from requests import get, post, put
-
-
 class ConfigBaseUrl(object):
     base_url = None
 
@@ -14,31 +8,3 @@ class ConfigHeaders(object):
 
 class ConfigPhotoUrl(object):
     photo = None
-
-
-json_body = json.dumps(
-    {
-        "id": 358,
-        "category":
-            {
-                "id": 819,
-                "name": "string"
-            },
-        "name": "doggie",
-        "photoUrls": [
-            "string"
-        ],
-        "tags": [
-            {
-                "id": 294,
-                "name": "string"
-            }
-        ],
-        "status": "available"
-    }
-)
-
-
-test_request = put("https://petstore.swagger.io/v2/pet", json=json_body)
-
-print(test_request.headers)
