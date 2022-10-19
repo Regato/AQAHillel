adee930be06d91f51feeb3cbefdd1fe8745a9c7a
Regato <shingamidesu@gmail.com>
2022-10-20 00:20:33 +0300

    HW22 fixed

--- HW22/file_read.py
+++ HW22/file_read.py
@@ -1,4 +1,4 @@
-import os
+import csv
 import pickle
 from io import BytesIO
 from typing import List
@@ -9,62 +9,22 @@ from user import User
 def csv_to_dict(path):
     """Returns User class objects list"""
 
-    # Check is file existing
-    if os.path.exists(path) is not True:
-        print('[CSV_TO_DICT]: CSV file is not existing!')
+    with open(path, 'r') as file:
+        csv_data = csv.DictReader(file)
+        csv_extract = [extract_data for extract_data in csv_data]
 
-        return False
-    else:
-        # If existing - open it
-        with open(path, 'r') as file:
-            # Read file by lines formatting
-            csv_lines = file.readlines()
+        user: List[User] = User.from_csv(csv_extract)
 
-            # Cut first line with headers information
-            first_line = csv_lines.pop(0)
-
-            # Making a dict from extracted data
-            user_data = dict()
-            user_data['headers'] = first_line
-            user_data['data'] = csv_lines
-
-            # Using from_csv method from User class
-            # to make a CSV file from data
-            users: List[User] = User.from_csv(user_data)
-
-            return users
+        print(user)
+        return user
 
 
-def user_to_bytes(path, function_obj):
+def user_to_bytes(path, function):
     """Returns True and writing bytes data from CSV to a bytes file."""
+    users = [user.__dict__ for user in function]
+    write_users = [list(user.values()) for user in users]
 
-    # Open file by a path
+    header_bytes = pickle.dumps(write_users)
+    head_buffer = BytesIO(header_bytes)
     with open(path, 'wb') as file:
-        # Making headers in bytes format
-        header_str = f'first_name last_name email gender'
-        header_bytes = pickle.dumps(header_str)
-        # Putting bytes headers into BytesIO container (buffer)
-        # to easily write it into a file
-        # (python does not provide this function)
-        head_buffer = BytesIO(header_bytes)
-        # Writing buffer data to a file
         file.writelines(head_buffer)
-
-        # Getting User class objects (users) from a list of users
-        for obj in function_obj:
-            # Making data from class object (user) in bytes format
-            data_str = f'{obj.firstAttribute} ' \
-                       f'{obj.secondAttribute} ' \
-                       f'{obj.thirdAttribute} ' \
-                       f'{obj.fourthAttribute}'
-            data_bytes = pickle.dumps(data_str)
-            # Putting bytes data into BytesIO container (buffer)
-            # to easily write it into a file
-            # (python does not provide this function)
-            data_buffer = BytesIO(data_bytes)
-            # Writing buffer data to a file
-            file.writelines(data_buffer)
-
-        print('[USER_TO_BYTES]: User class object are converted to a bytes file!')
-
-        return True
--- HW22/user.py
+++ HW22/user.py
@@ -20,70 +20,11 @@ class User:
         self.__gender = gender
 
     @classmethod
-    def from_csv(cls, csv_data: dict) -> List[User]:
-        # Getting headers and data from a dict
-        headers = csv_data['headers']
-        data = csv_data['data']
-        # Making lists from strings
-        headers_split = headers.split(' ')
-        pre_len = len(data)
-        data_split = [data[index].split(' ') for index in range(pre_len)]
-        # Taking length of lists
-        header_len = len(headers_split)
-        data_len = len(data_split)
+    def from_csv(cls, csv_line: dict) -> List[User]:
+        users = list()
 
-        # Making empty list to return it
-        user_list = list()
+        for line in csv_line:
+            user = User(**line)
+            users.append(user)
 
-        # Check are headers providing usable data
-        # Check is headers length equal data length
-        if data_len == 1 and header_len == 4:
-            # Making a new user from collected data
-            new_user = User(
-                first_name=data_split[0][0],
-                last_name=data_split[0][1],
-                email=data_split[0][2],
-                gender=data_split[0][3]
-            )
-
-            # Appending User class object (user) to a list of users
-            user_list.append(new_user)
-
-            print('[FROM_CSV]: User is created successfully!')
-        elif data_len > 1 and header_len == 4:
-            # Getting index of current list and make an operation
-            for index_list in range(data_len):
-                # Getting current list
-                current_list = data_split[index_list]
-                # Making a new user from collected data
-                new_user = User(
-                    first_name=current_list[0],
-                    last_name=current_list[1],
-                    email=current_list[2],
-                    gender=current_list[3]
-                )
-
-                # Appending User class object (user) to a list of users
-                user_list.append(new_user)
-
-            print('[FROM_CSV]: Users are created successfully!')
-        else:
-            print('[FROM_CSV]: Error, data from the CSV file are incorrect!')
-
-        return user_list
-
-    @property
-    def firstAttribute(self):
-        return self.__first_name
-
-    @property
-    def secondAttribute(self):
-        return self.__last_name
-
-    @property
-    def thirdAttribute(self):
-        return self.__email
-
-    @property
-    def fourthAttribute(self):
-        return self.__gender
+        return users
